7c869d32b601c262887c36eec6be8376ae3522de
hodol <ilyalysanuk123@gmail.com>
2026-06-07 21:30:16 +0700

    123

--- src/services/newsService.js
+++ src/services/newsService.js
@@ -1,14 +1,21 @@
-async function fetchNews() {
+const axios = require("axios");
+
+async function getNews(query) {
+  // Отправляем запрос на newsapi
   const response = await axios.get("https://newsapi.org/v2/everything", {
-    params: params
+    // Передаем в запрос:
+    params: {
+      q: query, // Что мы ищем
+      apiKey: process.env.NEWS_API_KEY, // api ключ
+      pageSize: 10, // Сколько новостей нужно
+    },
+    timeout: 5000,
   });
-  return response.data;
+
+  // Из всего ответа от newsapi нам нужны только articles (новости)
+  return response.data.articles;
 }
 
-// Или если у вас экспортируемая функция:
-module.exports = async function getNews() {
-  const response = await axios.get("https://newsapi.org/v2/everything", {
-    params: params
-  });
-  return response.data;
-};
+// Экспортируем функцию
+// Чтобы ее можно было вызвать из другого файла
+module.exports = { getNews };
